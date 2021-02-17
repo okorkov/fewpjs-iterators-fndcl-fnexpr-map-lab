465ff43deececc0cbd87b6fba5885fb201af12dd
okorkov <alex.okorkov@gmail.com>
2021-02-17 14:59:52 -0800

    commit tree fde67110e83b6c3fd81d56f5491efda96b01b0a0

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function capitalize(word) {
+  if (typeof word !== 'string') return word
+  return word.charAt(0).toUpperCase() + word.slice(1)
 }
+
+const tutorialsCaps = tutorials.map(tutorial => 
+  tutorial.split(' ').map(word => 
+    word = word[0].toUpperCase() + word.slice(1)).join(' '))
+
+
+const titleCased = () => {
+  return tutorialsCaps
+}
+
+console.log(titleCased())
